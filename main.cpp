b208b6396bfa1fbb121c3c640a334035fb0cb739
ultravio1encee <ksuuu2711@gmail.com>
2025-02-19 23:56:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include "huffman.h"
 
+int main() {
+    std::string inputFile = "haffman.txt";
+    std::string encodedFile = "compressed.huff";
+    std::string decodedFile = "decompressed.txt";
+
+    encodeFile(inputFile, encodedFile);
+    decodeFile(encodedFile, decodedFile);
+
+    return 0;
+}
